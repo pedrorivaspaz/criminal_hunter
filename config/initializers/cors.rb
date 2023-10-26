78583b4dc60e4c0f996c104dee54d23a66f26211
Pedro_rivas <pedro-rivas2003@hotmail.com>
2023-10-26 17:00:31 -0300

    fix error cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000", "https://criminal-hunter.vercel.app"
+    origins "http://localhost:3000", "https://criminal-hunter.vercel.app", "https://criminalhunter-production.up.railway.app/api-docs/index.html"
 
     resource "*",
       headers: :any,
